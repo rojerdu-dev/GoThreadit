7426d3a7e4d465e4d8231927ec5e3117835d6087
Rojer Du <rojerdu.dev@gmail.com>
2023-11-03 15:06:48 -0700

    Add database layer

--- postgres/thread_store.go
+++ postgres/thread_store.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/rojerdu-dev/gothreadit"
@@ -17,21 +18,51 @@ type ThreadStore struct {
 }
 
 func (s *ThreadStore) Thread(id uuid.UUID) (gothreadit.Thread, error) {
-	panic("not implemented") // TODO: implement
+	var t gothreadit.Thread
+	err := s.Get(&t, `SELECT * FROm threads WHERE id = $1`, id)
+	if err != nil {
+		return gothreadit.Thread{}, fmt.Errorf("error getting thread: %w", err)
+	}
+	return t, nil
 }
 
 func (s *ThreadStore) Threads() ([]gothreadit.Thread, error) {
-	panic("not implemented") // TODO: implement
+	var tt []gothreadit.Thread
+	err := s.Select(&tt, `SELECT * FROM threads`)
+	if err != nil {
+		return []gothreadit.Thread{}, fmt.Errorf("error getting threads: %w", err)
+	}
+	return tt, nil
 }
 
 func (s *ThreadStore) CreateThread(t *gothreadit.Thread) error {
-	panic("not implemented") // TODO: implement
+	err := s.Get(t, `INSERT INTO threads VALUES ($1, $2, $3) RETURNING *`,
+		t.Title,
+		t.Description,
+		t.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("error creating thread: %w", err)
+	}
+	return nil
 }
 
 func (s *ThreadStore) UpdateThread(t *gothreadit.Thread) error {
-	panic("not implemented") // TODO: implement
+	err := s.Get(t, `UPDATE threads SET title = $1, description = $2 WHERE id = $3 RETURNING *`,
+		t.ID,
+		t.Title,
+		t.Description,
+	)
+	if err != nil {
+		return fmt.Errorf("error updating thread: %w", err)
+	}
+	return nil
 }
 
 func (s *ThreadStore) DeleteThread(id uuid.UUID) error {
-	panic("not implemented") // TODO: implement
+	_, err := s.Exec(`DELETE FROM threads WHERE id = $1`, id)
+	if err != nil {
+		return fmt.Errorf("error deleting thread: %w", err)
+	}
+	return nil
 }
