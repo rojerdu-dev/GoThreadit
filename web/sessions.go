9b7aec67a0d20540d9b6bf88c55c6be96478f99b
Rojer Du <rojerdu.dev@gmail.com>
2023-11-15 13:43:52 -0800

    Add middleware and user login functionality

--- web/sessions.go
+++ web/sessions.go
@@ -3,9 +3,16 @@ package server
 import (
 	"context"
 	"database/sql"
+	"encoding/gob"
 	"github.com/alexedwards/scs/postgresstore"
 	"github.com/alexedwards/scs/v2"
+	"github.com/google/uuid"
+	"github.com/rojerdu-dev/gothreadit"
 )
+
+func init() {
+	gob.Register(uuid.UUID{})
+}
 
 func NewSessionManager(dataSourceName string) (*scs.SessionManager, error) {
 	db, err := sql.Open("postgres", dataSourceName)
@@ -22,14 +29,15 @@ func NewSessionManager(dataSourceName string) (*scs.SessionManager, error) {
 type SessionData struct {
 	FlashMessage string
 	Form         interface{}
-	// UserID uuid.UUID
+	User         gothreadit.User
+	LoggedIn     bool
 }
 
 func GetSessionData(session *scs.SessionManager, ctx context.Context) SessionData {
 	var data SessionData
 
 	data.FlashMessage = session.PopString(ctx, "flash")
-	// data.UserID, _ = session.Get(ctx, "user_id").(uuid.UUID)
+	data.User, data.LoggedIn = ctx.Value("user").(gothreadit.User)
 
 	data.Form = session.Pop(ctx, "form")
 	if data.Form == nil {
